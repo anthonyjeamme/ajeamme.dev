7d3a75fe19c01a7ecffbfd75076a7757afa590e0
tony <anthony@jeamme.fr>
2022-10-21 19:54:47 +0200

    Home Page | Minor visual fixes

--- src/Layout/TopBar/TopBar.tsx
+++ src/Layout/TopBar/TopBar.tsx
@@ -11,7 +11,6 @@ const className = classNameModule(styles);
 
 export const Topbar = () => {
   const rootRef = useRef<HTMLDivElement>(null);
-  const router = useRouter();
 
   useEffect(() => {
     const handleScroll = () => {
@@ -36,27 +35,29 @@ export const Topbar = () => {
   return (
     <div ref={rootRef} {...className("Topbar")}>
       <div>
-        <Link href={"/"}>
-          <img
-            src="/images/logo.svg"
-            className={styles["logo"]}
-            height={40}
-            width={40}
-            alt="Anthony JEAMME logo"
-          />
-        </Link>
-      </div>
+        <div>
+          <Link href={"/"}>
+            <img
+              src="/images/logo.svg"
+              className={styles["logo"]}
+              height={40}
+              width={40}
+              alt="Anthony JEAMME logo"
+            />
+          </Link>
+        </div>
 
-      <div>
-        {isMobile() ? (
-          <BurgerButton />
-        ) : (
-          <nav>
-            <a href="#competence">Compétences</a>
-            <a href="#experience">Expérience</a>
-            <a href="#contact">Contact</a>
-          </nav>
-        )}
+        <div>
+          {isMobile() ? (
+            <BurgerButton />
+          ) : (
+            <nav>
+              <a href="#competence">Compétences</a>
+              <a href="#experience">Expérience</a>
+              <a href="#contact">Contact</a>
+            </nav>
+          )}
+        </div>
       </div>
     </div>
   );
